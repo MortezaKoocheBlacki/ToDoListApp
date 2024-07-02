e0ca0a2038499f80570481e456237b5d1208e3f4
MortezaBlackie <mortezakoochemeshki81@gmail.com>
2024-07-02 06:29:10 -0700

    JavaScrit Codes updated and some animations are added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,11 +14,30 @@ userTasksInput.addEventListener("keyup", function(e){
 
 let addingItem = (UserTask) => {
       let taskItem = document.createElement("li");
-      taskItem.innerHTML = `<span>${UserTask}</span> <i class="fa-solid fa-circle-xmark"></i>`
+      taskItem.innerHTML = `<span>${UserTask}</span> <i class="fa-solid fa-circle-xmark removed"></i>`
+
+      taskItem.classList.add("added");
 
       taskItem.addEventListener("click", function() {
             this.classList.toggle("done");
       });
 
+      let removeIcon = taskItem.querySelector("i.removed");
+      removeIcon.addEventListener("click", function(e) {
+            // Stop event propagation to prevent toggling the "done" class
+            e.stopPropagation();
+
+            // Add class to trigger fade-out animation
+            taskItem.classList.add("removed");
+
+            // Wait for the animation to complete before removing the element
+            taskItem.addEventListener("transitionend", function() {
+                taskContainer.removeChild(taskItem);
+            }, { once: true });
+      });
+
       taskContainer.appendChild(taskItem);
-}
+
+      // Trigger reflow to ensure the animation runs
+      window.getComputedStyle(taskItem).opacity;
+};

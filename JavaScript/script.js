806252992b987a08d4db3f3db95e4164c02611b4
MortezaBlackie <mortezakoochemeshki81@gmail.com>
2024-07-02 00:03:52 -0700

    Base Of JavaScript Codes

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+let userTasksInput = document.querySelector(".wrapper .userTask"),
+taskContainer = document.querySelector(".tasks");
+
+userTasksInput.addEventListener("keyup", function(e){
+      if(e.key === "Enter"){
+            addingItem(userTasksInput.value);
+            this.value = "";
+      }
+});
+
+let addingItem = (UserTask) => {
+      let taskItem = document.createElement("li");
+      taskItem.innerHTML = `${UserTask} <i class="fa-solid fa-circle-xmark"></i>`
+
+      console.log(UserTask);
+
+      taskItem.addEventListener("click", function() {
+            this.classList.toggle("done");
+      });
+
+      taskContainer.appendChild(taskItem);
+}
